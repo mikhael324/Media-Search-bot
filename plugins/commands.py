48b81321b6c4976e8212c86e07ebc05fc6a8f765
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2022-01-07 13:55:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -99,12 +99,12 @@ async def start(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
-                        InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
-                    ],
-                    [
-                        InlineKeyboardButton("About", callback_data="about")
+                        
+                        InlineKeyboardButton("Movie Updates Channel", url="https://t.me/+f8itNAM31C00Mjc1")
                     ]
+                    
+                       
+                    
                 ]
             )
         )
