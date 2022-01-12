39588e052080524a83b039c5f085519ebd07a715
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2022-01-12 22:43:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -45,7 +45,7 @@ async def filter(client, message):
                 disable_web_page_preview=True
             )
             return
-    if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
+    if re.findall("^\/|^,|^!|^\.|^[\U0001F600-\U000E007F].*", message.text):
         return
     if 2 < len(message.text) < 100:    
         btn = []
