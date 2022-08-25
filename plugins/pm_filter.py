f8bfeb1d7758ff2b6198ff81ac674faa9e92a69c
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2022-08-25 21:45:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -90,7 +90,7 @@ async def filter(client, message):
             else:
                 k= await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
                 await asyncio.sleep(600)
-                await k.edit_text(f"<b>🗑️ Filter For {search} Is Closed ‼️</b>")
+                await k.edit_text(f"<b>🗑️ Filter Deleted After 10 Mins ‼️ \n 🔍Search Again !!</b>")
                
             return 
 
@@ -113,7 +113,7 @@ async def filter(client, message):
         else:
            k= await message.reply_text(f"<b>Hey 👋 Buddy 😎\n Your Search Results For:- {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
            await asyncio.sleep(600)
-           await k.edit_text(f"<b>🗑️ Filter For {search} Is Closed ‼️</b>")
+           await k.edit_text(f"<b>🗑️ Filter Deleted After 10 Mins ‼️ \n 🔍Search Again !!</b>")
                 
           
 
@@ -166,7 +166,7 @@ async def group(client, message):
               k= await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
               await asyncio.sleep(600)
 
-              await k.edit_text(f"<b>🗑️ Filter For {search} Is Closed ‼️</b>")
+              await k.edit_text(f"<b>🗑️ Filter Deleted After 10 Mins ‼️ \n 🔍Search Again !!</b>")
             return 
 
         data = BUTTONS[keyword]
@@ -188,7 +188,7 @@ async def group(client, message):
         else:
             k= await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             await asyncio.sleep(600)
-            await k.edit_text(f"<b>🗑️ Filter For {search} Is Closed ‼️</b>")
+            await k.edit_text(f"<b>🗑️ Filter Deleted After 10 Mins ‼️ \n 🔍Search Again !!</b>")
 
     
 def get_size(size):
